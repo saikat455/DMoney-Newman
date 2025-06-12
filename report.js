3f8640df865d4f46bb076544900944014da196bb
Abu sayed <saikat25112000@gmail.com>
2025-06-12 19:06:02 +0600

    Generate report

---
+++ report.js
@@ -0,0 +1,15 @@
+const newman = require('newman');
+
+newman.run({
+    collection: require('./collection.json'),
+    reporters: `htmlextra`,
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('Collection run complete!');
+});
